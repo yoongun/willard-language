b4e5b7613ca5029b22f3c5cbef2091ef16d8029f
cfr2ak <cfr2ak@gmail.com>
2019-10-25 10:56:52 +0900

    Add token types
    Add token types to use on lexer

---
+++ willard/token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type ToeknType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT"
+	EQUAL = "="
+	PLUS = "+"
+
+	// Delimiter
+	COMMA = ","
+	COLON = ":"
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+)
+
+
