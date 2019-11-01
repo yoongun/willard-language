83e2e1524edd9967319ee88dabeda24f639034c9
cfr2ak <cfr2ak@gmail.com>
2019-11-01 22:56:38 +0900

    Add switches
    Add switches to detect character or identifier and return it as a token

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "go/token"
+
 type Lexer struct {
 	input        string
 	position     int
@@ -21,4 +23,43 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhiteSpace()
+
+	switch l.ch {
+	case '|':
+		tok = newToken(token.BAR, l.ch)
+	case '>':
+		tok = newToken(token.RANGLE, l.ch)
+	case '<':
+		tok = newToken(token.LANGLE, l.ch)
+	case ':':
+		tok = newToken(token.COLON, l.ch)
+	case '(':
+		tok = newToken(token.LBRACE, l.ch)
+	case ')':
+		tok = newToken(token.RBRACE, l.ch)
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literalal)
+			return tok
+		} else if isDigit(l.chl) {
+			tok.Literal = l.readChar()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\r' {
+		l.readChar()
+	}
 }
